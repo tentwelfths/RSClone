f90743c00c44582ed0d6645a44cf69780c934b29
Christopher Knapp <Christopher Knapp>
2016-12-26 23:23:11 -0600

    Streamlined inventory manipulation

---
+++ RSClone/Assets/Scripts/ItemDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDictionary : MonoBehaviour {
+
+    public Inventory.Item[] items;
+   
+    // Use this for initialization
+    void Awake () {
+		for(int i = 0; i < items.Length; i++)
+        {
+            Inventory.importItem(items[i]);
+        }
+	}
+
+    void CreateItem(Inventory.Item _item)
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
