ff58486dbfaadf181aae08fb092903169546366e
Christopher Knapp <Christopher Knapp>
2017-01-05 19:38:51 -0600

    Revert "Campfire prefab"
    This reverts commit 0f68c5f9b3059d115d146af69af824d22951e95f.
    
    # Conflicts:
    #	RSClone/Assets/PathEditor/DynMapEditor.cs.meta
    #	RSClone/Assets/PathEditor/Sphere.prefab.meta
    #	RSClone/Library/CurrentLayout.dwlt
    #	RSClone/Library/CurrentMaximizeLayout.dwlt
    #	RSClone/Library/InspectorExpandedItems.asset
    #	RSClone/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	RSClone/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	RSClone/Library/assetDatabase3
    #	RSClone/Library/metadata/4d/4d69a87f471035d4f9968c4f78a9f1a3
    #	RSClone/Library/metadata/69/69ff25e7d77a1044aa3e6632b10947d5
    #	RSClone/Library/metadata/bb/bb88a564ede9f544ab007191a4393ea0.info

---
+++ RSClone/Assets/Scripts/Campfire.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campfire : MonoBehaviour {
+
+    public bool permanent = false;
+    public float despawnTime;
+    public CookingIO[] Cookables;
+
+
+
+    private float despawnCountdown;
+
+    private void Awake()
+    {
+        despawnCountdown = despawnTime;
+    }
+
+    private void Update()
+    {
+        if (!permanent)
+        {
+            despawnCountdown -= Time.deltaTime;
+            if (despawnCountdown <= 0.0f)
+                Destroy(gameObject);
+        }
+    }
+
+    public void Cook()
+    {
+        // Move player to campfire
+
+        // Iterate through list of cookable items
+        //   if player has the cookable item
+        //   remove it from inventory
+        //   roll chance to burn the item
+        //   add item(either burnt form or cooked form)
+        for (int i = 0; i < Cookables.Length; i++)
+        {
+            if (Inventory.inv.CheckForItem(Cookables[i].input))
+            {
+                // This is where the delay would go.
+                Inventory.inv.RemoveItem(Cookables[i].input);
+                if (Random.Range(0.0f, 1.0f) > Cookables[i].FailChance)
+                {
+                    Inventory.inv.addItem(Cookables[i].output);
+                    GamePlayLog.LogMessage(Cookables[i].SuccessMessage);
+                }
+                else
+                {
+                    Inventory.inv.addItem(Cookables[i].failOutput);
+                    GamePlayLog.LogMessage(Cookables[i].FailureMessage);
+                }
+                return;
+            }
+        }
+        GamePlayLog.LogMessage("You have nothing to cook!");
+    }
+
+    private void Feed()
+    {
+        if (Inventory.inv.CheckForItem("logs"))
+        {
+            // This is where the delay would go.
+            Inventory.inv.RemoveItem("logs");
+            despawnCountdown += 30.0f;
+            GamePlayLog.LogMessage("You toss some logs into the fire.");
+            return;
+        }
+        GamePlayLog.LogMessage("You have nothing to burn!");
+    }
+}
